16e65cf3cc1305ee4f410678ec83d2da06b4d9b2
yashborda7048 <yashborda123@gmail.com>
2023-10-16 08:24:50 +0530

    update find query

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -28,56 +28,66 @@ const technologySchema = new mongoose.Schema({
 const Technology = new mongoose.model("Technology", technologySchema);
 
 // create document or insert
-const createDocument = async () => {
-  try {
-    const JSData = new Technology({
-      name: "JS",
-      ctype: "Front end",
-      active: true,
-    });
+// const createDocument = async () => {
+//   try {
+//     const JSData = new Technology({
+//       name: "JS",
+//       ctype: "Back end",
+//       videos: 40,
+//       active: true,
+//     });
 
-    const VueData = new Technology({
-      name: "Vue",
-      ctype: "Front end",
-      active: true,
-    });
+//     const VueData = new Technology({
+//       name: "Vue",
+//       ctype: "Front end",
+//       videos: 50,
+//       active: true,
+//     });
 
-    const FigmaData = new Technology({
-      name: "Figma",
-      ctype: "Front end",
-      active: true,
-    });
+//     const FigmaData = new Technology({
+//       name: "Figma",
+//       ctype: "Front end",
+//       videos: 80,
+//       active: true,
+//     });
 
-    const MongoData = new Technology({
-      name: "Mongo DB",
-      ctype: "Database",
-      active: true,
-    });
+//     const MongoData = new Technology({
+//       name: "Mongo DB",
+//       ctype: "Database",
+//       videos: 120,
+//       active: true,
+//     });
 
-    const result = await Technology.insertMany([
-      JSData,
-      VueData,
-      FigmaData,
-      MongoData,
-    ]);
-    
-    console.log(result, "result");
-  } catch (err) {
-    console.log(err, "err");
-  }
-};
+//     const result = await Technology.insertMany([
+//       JSData,
+//       VueData,
+//       FigmaData,
+//       MongoData,
+//     ]);
+
+//     // console.log(result, "result");
+//   } catch (err) {
+//     console.log(err, "err");
+//   }
+// };
 
 // createDocument();
 
 const getDocument = async () => {
   try {
-    const result = await Technology.find({ctype: "Front end"})
-    .select({name: 1})  // only show {id, name}
-    .limit(1);  // only show first data
-    console.log(result, "result");
-  } catch(err) {
+    const result = await Technology
+      // .find({ ctype: { $eq: "Front end" } }) // only get match to "Front end"
+      // .find({ctype: "Front end"}) // only get match to "Front end"
+      // .find({videos: {$gt : 50}}) // only get greater than 50
+      // .find({ videos: { $gte: 50 } }) // only get greater than 50
+      // .find({ ctype: { $in: ['Back end', 'Database'] }}) // only get type in [Back end, Database] 
+      .find({ ctype: { $nin: ['Back end', 'Database'] }}) // only get type in not [Back end, Database] 
+      .select({ name: 1 }); // only show {id, name}
+    // .limit(1);  // only show first data
+    console.log(result, "find");
+  } catch (err) {
     console.log(err, "err");
   }
-}
+};
 
-getDocument();
+// getDocument();
